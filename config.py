7b2f65640cbd1b9137eb5b2da2aafc8ce2b17a8f
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-10-05 19:03:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_NAME = getenv("OWNER_NAME", "Andreobee_xD")
 # fill with DISABLE if you want to disable pm-permit
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 # fill with your id as the owner of the bot
-OWNER_ID = int(os.environ.get("OWNER_ID"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1958076591"))
 DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
 # make a private channel and get the channel id
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001518820308"))
