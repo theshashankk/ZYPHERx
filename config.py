24fa4e55c762c857486fa61796303352e5fc9c44
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-10-19 12:09:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ load_dotenv()
 que = {}
 admins = {}
 SESSION_NAME = getenv("SESSION_NAME", "AQAXycVEHNyZQZmQYSJCWhv2Up4MB9rNWfEZN-9BWvqEaZlZlo-FsW4zVbtAiR_wqtppgBgTVeBQ1ZCW3inHzPK9a8hTnpdUh_AhpYh_VucvxnOuKycbDCNjjDksSXimhG39fsIjUyhuqVqS-PvOeEmHSk267gLQ3MVe6IMGk_7w6rQihFj0Ct1qVdSARpaPwwvSo18S06rxiPlXJsXpLNOs8k5AzfDyWB6mgSUCPzuBldMAK0yfGYm1q62IWJkoX8Cyt2nznNlBN1GZa3CSc0GA5BqEzQE6OJWPxgQWu5jTcNYY1Iqgxw-5aztlfQVZONhH7f9H7lvbIyawFiJDby-tdquPPgA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1988131788:AAHds6rsEEAYLJt6TlS5Kic74KHSmgKunBo")
+BOT_TOKEN = getenv("BOT_TOKEN", "2061760260:AAH5oGUxogL5ZkUTX4yctjSqEFXN4aNyi7E")
 BOT_NAME = getenv("BOT_NAME", "ZYPHERx_BOT")
 BG_IMAGE = getenv("BG_IMAGE", "https://raw.githubusercontent.com/theshashankk/HinataBot/main/background1.png")
 THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/976bb4ee2f66857cfa317.jpg")
